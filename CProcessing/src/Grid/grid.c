bd63da9f8ec4dfb11ddbeb13dc1f35b00b10c319
PwniieLap <jiarong_phang@yahoo.com>
2020-10-12 19:54:59 +0800

    Documentation for grid.c added
    Signed-off-by: PwniieLap <jiarong_phang@yahoo.com>

--- CProcessing/src/Grid/grid.c
+++ CProcessing/src/Grid/grid.c
@@ -2,46 +2,54 @@
 #include <stdlib.h>
 #include <cprocessing.h>
 
-#define GRID_WIDTH 50
-#define GRID_HEIGHT 50
+/*
+This is for casting a grid
+*/
 
-#define GRID_SIZE GRID_WIDTH*GRID_HEIGHT
+#define GRID_WIDTH 50				//Set the horizontal length
+#define GRID_HEIGHT 50				//Set the vertical length
 
+#define GRID_SIZE GRID_WIDTH*GRID_HEIGHT		//Total size
 
+//Different type of elements store for the array;
 typedef enum
 {
-	GE_VOID,
-	GE_WALL,
-	GE_SNAKE,
-	GE_FOOD
+	GE_VOID,		//Empty
+	GE_WALL,		//Wall
+	GE_SNAKE,		//Snake
+	GE_FOOD			//Food
 
-}GRID_ELEMENTS;
+}GRID_ELEMENTS;	//@todo Might want to relocate to header file
 
-GRID_ELEMENTS grid[GRID_SIZE];
+//Grid that stores the element type
+GRID_ELEMENTS grid[GRID_SIZE];	//@todo Might want to relocate to snake, or I can make a getter to return this variable
 
 
+//Initialize Grid
 void Grid_Init(void) 
 {
 	//Boundary
 	for (int i = 0; i < GRID_SIZE; i++) {
 
-		if (i < GRID_WIDTH
-			|| (i % GRID_WIDTH) == 0
-			|| (i % GRID_WIDTH) == GRID_WIDTH - 1
-			|| i >= (GRID_HEIGHT - 1) * GRID_WIDTH  && i < GRID_SIZE
+		if (	i < GRID_WIDTH												/*Top Boundary*/
+			|| (i % GRID_WIDTH) == 0										/*Left Boundary*/
+			|| (i % GRID_WIDTH) == GRID_WIDTH - 1							/*Right Boundary*/
+			|| i >= (GRID_HEIGHT - 1) * GRID_WIDTH  && i < GRID_SIZE		/*Bottom Boundary*/
 			) {
-			grid[i] = GE_WALL;
+			grid[i] = GE_WALL;												//Set Grid element to wall
 		}
 		else 
 		{
-			grid[i] = GE_VOID;
+			grid[i] = GE_VOID;												//Empty Element
 		}
 		
 	}
 }
 
+//Draw Call/Update Function for Grid
 void Grid_Update(void)
 {
+	//Checks thru all the elements
 	for (int i = 0; i < GRID_SIZE; i++) 
 	{
 		switch (grid[i]) 
@@ -61,11 +69,3 @@ void Grid_Update(void)
 	}
 	
 }
-
-
-
-
-/*CP_Vector *CreateGridWRWindow(int size, int width, int height) {
-	
-	
-}*/
