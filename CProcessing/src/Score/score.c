56aeb36f94d65db67e72988e74d5ab235c9070f1
AceTreinerLeo <leonardkoh@yahoo.com>
2020-10-18 13:56:31 +0800

    Updated display score function
    Signed-off-by: AceTreinerLeo <leonardkoh@yahoo.com>

--- CProcessing/src/Score/score.c
+++ CProcessing/src/Score/score.c
@@ -1,6 +1,6 @@
 #include "../../CProcessing/inc/cprocessing.h"
 #include "score.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 int AddScore(void) //function for time-based score
 {
@@ -10,13 +10,11 @@ int AddScore(void) //function for time-based score
 
 void DisplayScore(int score) //function to display the score on screen
 {
-	char scoreBuffer[7]; //separate buffer for value of score
-	_itoa_s(score, scoreBuffer, 7, 10); //convert score value to string
-	char scoreString[15] = {'S','c','o','r','e',':','\0'}; //buffer for full string
-	strcat_s(scoreString, 15, scoreBuffer); //adds score value to the end of "Score: "
-	
-	CP_Color textColor = CP_Color_Create(0, 255, 255, 255); //set text to white
+	char scoreBuffer[15]; //buffer for value of score
+	sprintf_s(scoreBuffer, 15, "Score: %d", score); // print score into buffer
+
+	CP_Color textColor = CP_Color_Create(0, 255, 255, 255); //set text to white, size to 20
 	CP_Settings_Fill(textColor);
 	CP_Settings_TextSize(20);
-	CP_Font_DrawText(scoreString, 30.f, 40.f); //display score
+	CP_Font_DrawText(scoreBuffer, 30.f, 40.f); //display score at top left corner
 }
