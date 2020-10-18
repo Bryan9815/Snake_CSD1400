41de8817bb4e05fdf7bac49e15c915165187f5cc
legendarydeadweight <necropsychopath04@gmail.com>
2020-10-19 03:17:43 +0800

    Added basic structure for snake and movement

--- CProcessing/src/Snake/snake.c
+++ CProcessing/src/Snake/snake.c
@@ -6,6 +6,18 @@
 #include "../Food/food.h"
 #include "../Score/score.h"
 
+typedef struct
+{
+	CP_Vector	snakePos;
+	int			radius;
+	float		speed;
+	direction	dir;
+
+}snake;
+
+CP_Color snakeColor;
+snake s;
+
 int foodPosition;
 int Score;
 CP_Color bgColor;
@@ -20,6 +32,53 @@ void Snake_Draw(void)
 	Grid_Update(grid);
 }
 
+void Snake_Display(void)
+{
+	CP_Settings_Fill(snakeColor);
+	CP_Settings_EllipseMode(CP_POSITION_CENTER);
+	CP_Graphics_DrawCircle((s.snakePos.x / GRID_WIDTH) * CP_System_GetWindowWidth(),
+		(s.snakePos.y / GRID_HEIGHT) * CP_System_GetWindowHeight(),
+		(float)s.radius);
+}
+
+void Snake_Create(void)
+{
+	snakeColor = CP_Color_Create(255, 100, 100, 255);
+	s.snakePos.x = GRID_WIDTH / 2;
+	s.snakePos.y = GRID_HEIGHT / 2;
+	s.radius = 40;
+	s.dir = CP_Random_RangeInt(1, 4);
+	s.speed = CP_System_GetDt() * 2;
+}
+
+void Snake_Movement(void)
+{
+
+	if (CP_Input_KeyTriggered(KEY_UP) && (s.dir != DOWN))
+	{
+		s.dir = UP;
+		s.snakePos.y -= 1;
+	}
+
+	else if (CP_Input_KeyTriggered(KEY_DOWN) && (s.dir != UP))
+	{
+		s.dir = DOWN;
+		s.snakePos.y += 1;
+	}
+
+	else if (CP_Input_KeyTriggered(KEY_LEFT) && (s.dir != RIGHT))
+	{
+		s.dir = LEFT;
+		s.snakePos.x -= 1;
+	}
+
+	else if (CP_Input_KeyTriggered(KEY_RIGHT) && (s.dir != LEFT))
+	{
+		s.dir = RIGHT;
+		s.snakePos.x += 1;
+	}
+	
+}
 
 void snake_init(void)
 {
@@ -27,11 +86,14 @@ void snake_init(void)
 	spawn_food(foodPosition);
 	Score = 0;
 	Grid_Init(grid);
+	Snake_Create();
 }
 
 void snake_update(void)
 {
 	Snake_Draw();
+	Snake_Display();
+	Snake_Movement();
 	Score += AddScore();
 	DisplayScore(Score);
 }
--- CProcessing/src/Snake/snake.h
+++ CProcessing/src/Snake/snake.h
@@ -1,5 +1,13 @@
 #pragma once
 
+typedef enum
+{
+	LEFT = 1,
+	RIGHT,
+	UP,
+	DOWN
+} direction;
+
 void snake_init(void);
 void snake_update(void);
 void snake_exit(void);
