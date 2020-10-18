6ba19ab6a9615ebb207ea54e5dbe961b11e557dc
legendarydeadweight <necropsychopath04@gmail.com>
2020-10-19 01:23:21 +0800

    Added file for main menu

---
+++ CProcessing/src/Snake/MainMenu.c
@@ -0,0 +1,16 @@
+#include "MainMenu.h"
+
+void MainMenu_init(void)
+{
+
+}
+
+void MainMenu_update(void)
+{
+
+}
+
+void MainMenu_exit(void)
+{
+
+}
---
+++ CProcessing/src/Snake/MainMenu.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void MainMenu_init(void);
+void MainMenu_update(void);
+void MainMenu_exit(void);
